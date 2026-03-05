c7ef591c5bf182624e011c7561f7b53cc7134a79
basileusxx <113246033+basileusxx@users.noreply.github.com>
2026-03-06 00:57:00 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:56:29.467Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T21:57:01.260Z
+Commit: Patch security vulnerability in authentication
