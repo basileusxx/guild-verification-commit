ba6ebaa683da8b3c1287940a736b4a7275e2549a
basileusxx <113246033+basileusxx@users.noreply.github.com>
2026-03-06 00:56:10 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:55:41.727Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T21:56:10.702Z
+Commit: Fix broken links in documentation
