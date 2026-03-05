b0eb91b2b5d5a6aa8f3fda832cbf69a2925cad88
basileusxx <113246033+basileusxx@users.noreply.github.com>
2026-03-06 00:56:13 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:56:12.073Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T21:56:13.595Z
+Commit: Resolve null pointer exception in user service
