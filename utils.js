da916718c13f0c259cca837eb76c19d8cc4aa6c1
basileusxx <113246033+basileusxx@users.noreply.github.com>
2026-03-06 00:56:28 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T21:55:40.067Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T21:56:29.467Z
+Commit: Fix CSS styling bug on mobile devices
